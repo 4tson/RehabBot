14b2eaa7d7e4fb5303a39fca992bb668b57bbb66
Jorge Fortson <Jorge Fortson@DESKTOP-FRKSC7O>
2021-04-29 10:27:02 -0500

    Validations and flavour text when dueling or gift chucking the bot

--- rehab-bot/src/main/java/mx/fortson/rehab/channels/DuelArena.java
+++ rehab-bot/src/main/java/mx/fortson/rehab/channels/DuelArena.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.apache.commons.lang3.StringUtils;
 
+import mx.fortson.rehab.RehabBot;
 import mx.fortson.rehab.enums.ChannelsEnum;
 import mx.fortson.rehab.enums.RehabCommandsEnum;
 import mx.fortson.rehab.listeners.DuelStateMachine;
@@ -57,6 +58,10 @@ public class DuelArena implements IChannel{
 		if(splitContent.length==3) {
 			if(splitContent[1].startsWith("<@") && FormattingUtils.isValidAmount(splitContent[2])) {
 				Long giftedId = event.getMessage().getMentionedUsers().get(0).getIdLong();
+				if(giftedId.equals(RehabBot.getApi().getSelfUser().getIdLong())) {
+					channel.sendMessage("I am flattered, alas I am but a simple bot, what would I even do with money?").queue();
+					return;
+				}
 				Long giftChuckAmount = FormattingUtils.parseAmount((splitContent[2]));
 				Long gifterId = author.getIdLong();
 				channel.sendMessage(MessageUtils.getDuelResult(DuelUtils.giftChuck(gifterId,giftedId,giftChuckAmount))).allowedMentions(new ArrayList<>()).queue();
@@ -89,7 +94,9 @@ public class DuelArena implements IChannel{
 			if(splitContent[1].startsWith("<@") && FormattingUtils.isValidAmount(splitContent[2])) {
 				Member challengedMember = event.getMessage().getMentionedMembers().get(0);
 				Long challengedId = challengedMember.getIdLong();
-				if(challengedId.equals(author.getIdLong())) {
+				if(challengedId.equals(RehabBot.getApi().getSelfUser().getIdLong())) {
+					channel.sendMessage("I have been programmed to never lose. I don't think you want to test that.").queue();
+				}else if(challengedId.equals(author.getIdLong())) {
 					channel.sendMessage("You try to play with yourself... It's not very effective.").queue();
 				}else{
 					Long amount = FormattingUtils.parseAmount(splitContent[2]);
