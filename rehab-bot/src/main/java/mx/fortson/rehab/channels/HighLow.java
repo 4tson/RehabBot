77ca542fe08bc99ef7892b841eaf9979dff79a71
Jorge Fortson <Jorge Fortson@DESKTOP-FRKSC7O>
2021-05-03 17:29:51 -0500

    Using emojis for high-low to make the current number more obvious

--- rehab-bot/src/main/java/mx/fortson/rehab/channels/HighLow.java
+++ rehab-bot/src/main/java/mx/fortson/rehab/channels/HighLow.java
@@ -1,6 +1,7 @@
 package mx.fortson.rehab.channels;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,8 +28,11 @@ public final class HighLow implements IChannel{
 	private static int number;
 	private static TimerTask highLowTask = null;
 	private static Timer timer = new Timer("HighLowTimer");
+	private static final Map<Integer,String> NUMBERS_EMOJIS = createMap();
 
 	static{
+		
+		
 		playersBets = new HashMap<>();
 		playersWins = HighLowUtils.loadState();;
 		number = RandomUtils.randomInt(13);
@@ -85,6 +89,18 @@ public final class HighLow implements IChannel{
 		}
 	}
 	
+	private static Map<Integer, String> createMap() {
+		Map<Integer,String> result = new HashMap<>();
+		for(int i = 1; i<=9; i++) {
+			result.put(i, i + "\u20E3");
+		}
+		result.put(10, "1\u20E30\u20E3");
+		result.put(11, "1\u20E31\u20E3");
+		result.put(12, "1\u20E32\u20E3");
+		result.put(13, "1\u20E33\u20E3");
+		return Collections.unmodifiableMap(result);
+	}
+
 	protected CharSequence generateGameEndMessage(int oldNumber) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("The previous number was `")
@@ -183,11 +199,11 @@ public final class HighLow implements IChannel{
 		.append(">: ");
 		if(playersWins.containsKey(idLong)) {
 			PlayerWinsBean playerWins = playersWins.get(idLong);
-			sb.append("You could cash out ")
+			sb.append("You could cash out `")
 			.append(playerWins.cashOut())
-			.append(" farm(s). Your current rate is ")
+			.append("` farm(s). Your current rate is `")
 			.append(String.format("%.2f", playerWins.getRate()))
-			.append(" Every game that gets played without you participating or cashing out lowers your rate by .30, winning a game ups it by .10 up capped at .10 and 2 respectively.");
+			.append("`");
 		}else {
 			sb.append("You don't have any farms to cash out.");
 		}
@@ -201,18 +217,17 @@ public final class HighLow implements IChannel{
 		StringBuilder sb = new StringBuilder();
 		sb.append("<@")
 		.append(id)
-		.append(">: Your bet for ")
+		.append(">: Your bet for `")
 		.append(b ? "High" : "Low")
-		.append(" has been registered.");
+		.append("` has been registered.");
 		return sb.toString();
 	}
 
 	private static CharSequence generateNewNumberMessage() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("New number is ")
-		.append("`")
-		.append(number)
-		.append("` type `!high` or `!low` to gamble on the next number");
+		.append(NUMBERS_EMOJIS.get(number))
+		.append(" type `!high` or `!low` to gamble on the next number");
 		return sb.toString();
 	}
 
@@ -220,9 +235,9 @@ public final class HighLow implements IChannel{
 		StringBuilder sb = new StringBuilder();
 		sb.append("<@")
 		.append(idLong)
-		.append("> already has a bet for ")
+		.append("> already has a bet for `")
 		.append(playersBets.get(idLong) ? "High" : "Low")
-		.append(". Wait for the current game to end to keep playing.");
+		.append("`. Wait for the current game to end to keep playing.");
 		return sb.toString();
 	}
 
